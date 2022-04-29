d23ab14c201631e6c39b4c784928ace5ddafbde6
CarolynnFleming <cfcatpink@gmail.com>
2022-04-29 15:59:01 -0700

    charactercard

--- src/components/Character/Card.js
+++ src/components/Character/Card.js
@@ -1,3 +1,10 @@
-export default function CharacterCard({ benders }) {
-    return <span>{name}</span>
+export default function CharacterCard({ name, allies, photoUrl, affiliation }) {
+    return(
+        <div>
+            <p>{name}</p>
+            <img src={photoUrl}/>
+            <p>{allies}</p>
+            <p>{affiliation}</p>
+        </div>
+    )
 }
