5feba27c22c47b8a70ba5fedd467a863b07511d0
CarolynnFleming <cfcatpink@gmail.com>
2022-05-09 15:36:25 -0700

    change event for search

--- src/components/Character/Search.js
+++ src/components/Character/Search.js
@@ -3,6 +3,13 @@ import { useState } from "react";
 
 export default function Search({ onSearch }) {
     const [finder, setfinder] = useState('');
+
+    const change = (event) => {
+        setfinder(event.target.value);
+        onSearch(event.target.value);
+    };
+
+    
   return (
     <div>Search</div>
   )
