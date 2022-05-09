81b61eeeb78a5d5f760e9ef46e394ab44b90ec2e
CarolynnFleming <cfcatpink@gmail.com>
2022-05-09 15:30:12 -0700

    rewrote syntax getting multiple characters

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './views/Characters/Detail';
+import List from './views/Characters/List';
+
+
+export default function App() {
+  return (
+    <Switch>
+        <Route path="/character/:id">
+            <Detail />
+        </Route>
+        <Route path="/">
+            <List />
+        </Route>
+    </Switch>
+  )
+}
--- src/services/avatarApi.js
+++ src/services/avatarApi.js
@@ -1,11 +1,24 @@
 export async function fetchCharacter(id) {
     const res = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters`);
-const { _id, name, afilliation, allies, photoUrl} = await res.json();
-    return {
+const { bender } = await res.json();
+const { _id, name, afilliation, allies, photoUrl} = bender;
+      return{ 
         id: _id,
         name: name,
         image: photoUrl,
         afilliation: afilliation,
         allies: allies
     };
+}
+
+export async function fetchCharacters() {
+    const res = await fetch('https://last-airbender-api.herokuapp.com/api/v1/characters');
+    const { bender } = await res.json();
+    return bender.map(({_id, name, afilliation, allies, photoUrl}) => ({
+        id: _id,
+        name: name,
+        image: photoUrl,
+        afilliation: afilliation,
+        allies: allies
+    }));
 }
